97e317f6b0ee9c610a1a11655f9bae0be7f5a986
yemao8972 <137700665+yemao8972@users.noreply.github.com>
2023-06-25 21:25:45 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Hello World111</Title>
+      <h1>Hello world111!</h1>
       <Counter />
       <p>
         Visit{" "}
