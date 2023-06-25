8135b29d80a83c6e601388e53904342b034af3c7
yemao8972 <137700665+yemao8972@users.noreply.github.com>
2023-06-26 01:49:35 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,61 +5,61 @@ export default function Home() {
     <main>
       <Title>PRIVACY POLICY</Title>
       <h1>PRIVACY POLICY</h1>
-		<p><br>
-		Thank you<br>
-		for playing our games!<br><br>
+		<p>
+		Thank you<br/>
+		for playing our games!<br/><br/>
 
-		This Privacy Policy describes the way we collect and user your Information and<br>
-		informs you of your choices in relation to your Information.<br>
+		This Privacy Policy describes the way we collect and user your Information and<br/>
+		informs you of your choices in relation to your Information.<br/>
 
-		We do not collect personally identifiable information about you or your child through our<br>
-		applications. In other words, we do not collect information such as your name,<br>
-		address, phone number or email address.<br><br>
+		We do not collect personally identifiable information about you or your child through our<br/>
+		applications. In other words, we do not collect information such as your name,<br/>
+		address, phone number or email address.<br/><br/>
 
-		We do not have an account registration function in game, so we do not collect any of you<br>
-		registration information.<br><br>
+		We do not have an account registration function in game, so we do not collect any of you<br/>
+		registration information.<br/><br/>
 
-		We understand the importance of our users' personal information and, more<br>
-		importantly, information relating to children under the age of 13. We do not<br>
-		collect or require users to enter personal information when using our Products.<br>
-		We do not collect any personal information from children with our Products.<br><br>
+		We understand the importance of our users' personal information and, more<br/>
+		importantly, information relating to children under the age of 13. We do not<br/>
+		collect or require users to enter personal information when using our Products.<br/>
+		We do not collect any personal information from children with our Products.<br/><br/>
 
-		We do not have a payment function in game, so we do not collect any of your payment<br>
-		information.<br><br>
+		We do not have a payment function in game, so we do not collect any of your payment<br/>
+		information.<br/><br/>
 
-		We do not have Push Notifications and Local Notifications functional in game, so we do<br>
-		not collect any of your notification information.<br><br>
+		We do not have Push Notifications and Local Notifications functional in game, so we do<br/>
+		not collect any of your notification information.<br/><br/>
 
-		We do not have the function of using location information in game, so we do not collect<br>
-		any of your location information.<br><br>
+		We do not have the function of using location information in game, so we do not collect<br/>
+		any of your location information.<br/><br/>
 
-		We do not collect personally identifiable information from children under the age of 13.<br>
-		In the event that we discover that a child under the age of 13 has provided me<br>
-		with personal information, we will immediately delete this information from our<br>
-		servers. If you are a parent or guardian and you know that your child has<br>
-		provided us with personal information, please contact me so I can take the<br>
-		necessary actions.<br><br>
+		We do not collect personally identifiable information from children under the age of 13.<br/>
+		In the event that we discover that a child under the age of 13 has provided me<br/>
+		with personal information, we will immediately delete this information from our<br/>
+		servers. If you are a parent or guardian and you know that your child has<br/>
+		provided us with personal information, please contact me so I can take the<br/>
+		necessary actions.<br/><br/>
 
-		We comply with the Children's Online Privacy Protection Act(COPPA).<br><br>
+		We comply with the Children's Online Privacy Protection Act(COPPA).<br/><br/>
 
-		Our website and mobile applications comply with the Children's Online Privacy<br>
-		Protection Act ("COPPA").<br><br>
+		Our website and mobile applications comply with the Children's Online Privacy<br/>
+		Protection Act ("COPPA").<br/><br/>
 
-		In the event a user identifies himself as a child under the age of 13 through a<br>
-		support request or feedback, we will not collect, store or use it and will<br>
-		securely delete any personal information of that user.<br><br>
+		In the event a user identifies himself as a child under the age of 13 through a<br/>
+		support request or feedback, we will not collect, store or use it and will<br/>
+		securely delete any personal information of that user.<br/><br/>
 
-		Changes to This Privacy Policy<br><br>
+		Changes to This Privacy Policy<br/><br/>
 
-		We may update our Privacy Policy from time to time. Thus, you are advised to review<br>
-		this page periodically for any changes. We will notify you of any changes by<br>
-		posting the new Privacy Policy on this page.<br><br>
+		We may update our Privacy Policy from time to time. Thus, you are advised to review<br/>
+		this page periodically for any changes. We will notify you of any changes by<br/>
+		posting the new Privacy Policy on this page.<br/><br/>
 
-		Contact US<br><br>
+		Contact US<br/><br/>
 
-		If you have any questions about PRIVACY-POLICY, please send email to ystgpcudqrmo@gmail.com<br><br>
+		If you have any questions about PRIVACY-POLICY, please send email to ystgpcudqrmo@gmail.com<br/><br/>
 
-		Thanks.<br>
+		Thanks.<br/>
     </main>
   );
 }
