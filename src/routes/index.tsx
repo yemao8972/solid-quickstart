8c36c3f64bed063a1455d7f419b3b4deea3e7af3
yemao8972 <137700665+yemao8972@users.noreply.github.com>
2023-06-26 01:38:35 +0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,13 +5,64 @@ export default function Home() {
     <main>
       <Title>Hello World</Title>
       <h1>Hello world111!</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+		<p>
+		Thank you
+		for playing our games!
+
+		This Privacy Policy describes the way we collect and user your Information and
+		informs you of your choices in relation to your Information.
+
+
+		We do not collect personally identifiable information about you or your child through our
+		applications. In other words, we do not collect information such as your name,
+		address, phone number or email address.
+
+
+		We do not have an account registration function in game, so we do not collect any of you
+		registration information.
+
+		We understand the importance of our users' personal information and, more
+		importantly, information relating to children under the age of 13. We do not
+		collect or require users to enter personal information when using our Products.
+		We do not collect any personal information from children with our Products.
+
+		We do not have a payment function in game, so we do not collect any of your payment
+		information.
+
+		We do not have Push Notifications & Local Notifications functional in game, so we do
+		not collect any of your notification information.
+
+		We do not have the function of using location information in game, so we do not collect
+		any of your location information.
+
+		We do not collect personally identifiable information from children under the age of 13.
+		In the event that we discover that a child under the age of 13 has provided me
+		with personal information, we will immediately delete this information from our
+		servers. If you are a parent or guardian and you know that your child has
+		provided us with personal information, please contact me so I can take the
+		necessary actions.
+
+		We comply with the Children's Online Privacy Protection Act(COPPA).
+
+		Our website and mobile applications comply with the Children's Online Privacy
+		Protection Act ("COPPA").
+
+		In the event a user identifies himself as a child under the age of 13 through a
+		support request or feedback, we will not collect, store or use it and will
+		securely delete any personal information of that user.
+
+		Changes to This Privacy Policy
+
+		We may update our Privacy Policy from time to time. Thus, you are advised to review
+		this page periodically for any changes. We will notify you of any changes by
+		posting the new Privacy Policy on this page.
+
+		Contact US
+
+		If you have any questions about PRIVACY-POLICY, please send email to ystgpcudqrmo@gmail.com
+
+		Thanks.
+		</p>
     </main>
   );
 }
